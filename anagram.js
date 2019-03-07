1e10260ed3d705457453f1111d7ad97b526e201e
Felipe Piacsek <felipe.piacsek@gmail.com>
2019-03-07 14:29:56 -0300

    Displays letters diff.

--- anagram.js
+++ anagram.js
@@ -12,6 +12,19 @@ var Anagrams = function(){
         return !!word && typeof word === "string";
     };
 
+    var lettersMap = function(word) {
+        var letters = removeBlanks(word).toLowerCase().split("");
+        var map = {};
+        for(var i = 0; i < letters.length; i++){
+            if (!map[letters[i]]){
+                map[letters[i]] = 1;
+            }else{
+                map[letters[i]]++;
+            }
+        }
+        return map;
+    };
+
     // Exposures
     var isAnagram = function(referenceWord, word){
         if (!(validWord(referenceWord) && validWord(word))) return false;
@@ -22,7 +35,27 @@ var Anagrams = function(){
         return sortString(referenceWord) === sortString(word);
     };
 
+    var lettersDiffMap = function(referenceWord, word) {
+        var referenceWordLetterUsage = lettersMap(referenceWord);
+        var referenceLetters = Object.keys(referenceWordLetterUsage);
+        var wordLetterUsage = lettersMap(word);
+        var diffMap = {};
+        for(var i = 0; i < referenceLetters.length; i++){
+            var letter = referenceLetters[i];
+            var referenceAmount = referenceWordLetterUsage[letter];
+            var amountUsed = wordLetterUsage[letter];
+            if (isNaN(amountUsed)){
+                diffMap[letter] = referenceAmount;
+            }else{
+                diffMap[letter] = referenceAmount - amountUsed;
+            }
+        }
+
+        return diffMap;
+    };
+
     return {
-        isAnagram: isAnagram
+        isAnagram: isAnagram,
+        lettersDiffMap: lettersDiffMap
     };
 }();
--- index.scripts.js
+++ index.scripts.js
@@ -1,11 +1,20 @@
-var checkForAnagram = function(){
-    var word = document.getElementById("word").value;
-    var referenceWord = document.getElementById("referenceWord").value;
-
+var checkForAnagram = function(referenceWord, word){
     document.getElementById("anagramStatus").value = Anagrams.isAnagram(referenceWord, word);
 };
+
+var displayLettersDiff = function(referenceWord, word){
+    var diffMap = Anagrams.lettersDiffMap(referenceWord, word);
+    document.getElementById("lettersDiff").textContent = JSON.stringify(diffMap, null, 4);
+};
+
+var letterTypedDigest = function(){
+    var referenceWord = document.getElementById("referenceWord").value;
+    var word = document.getElementById("word").value;
+    checkForAnagram(referenceWord, word);
+    displayLettersDiff(referenceWord, word);
+};
+
 window.addEventListener('load', function(){
-    document.getElementById("word").addEventListener("keyup", checkForAnagram);
-    document.getElementById("referenceWord").addEventListener("keyup", checkForAnagram);
-    document.getElementById("anagramStatus").addEventListener("keyup", checkForAnagram);
+    document.getElementById("word").addEventListener("keyup", letterTypedDigest);
+    document.getElementById("referenceWord").addEventListener("keyup", letterTypedDigest);
 });
